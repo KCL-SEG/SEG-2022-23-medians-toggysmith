26b11083dfa2a5445a189fdedce02a6f646efbdd
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-09-29 15:58:25 +0100

    Return median of list
    Completed the coding exercise so that when a list of numbers is given as input to the program, a single number (the median of the numbers in the list) is given as output.

--- median.py
+++ median.py
@@ -5,8 +5,17 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+
+        numbers.sort()
+
+        if len(numbers) % 2 == 0: # If even
+            first_number = numbers[len(numbers) // 2 - 1]
+            second_number = numbers[len(numbers) // 2]
+
+            print((first_number + second_number) / 2)
+        else: # If odd:
+            print(numbers[len(numbers) // 2])
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
